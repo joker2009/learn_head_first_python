9cf804e94a7948645e08ff6eaaf5db13f2ab89f2
joker2009 <304640509@qq.com>
2017-11-29 13:34:08 +0800

    fist commit

--- head_first_1.py
+++ head_first_1.py
@@ -7,11 +7,21 @@ movies = ['the Holy Grail', 1975, 'Terry Jones', 91,
 # for each_item in movies:
 #     print(each_item)
 
-for each_item in movies:
-    if isinstance(each_item, list):
-        for nested_item in each_item:
-            print(nested_item)
-    else:
-        print(each_item)
+# for each_item in movies:
+#     if isinstance(each_item, list):
+#         for nested_item in each_item:
+#             print(nested_item)
+#     else:
+#         print(each_item)
 
 # 循环迭代
+
+# def print_lol(the_list):
+#     for each_item in the_list:
+#         if isinstance(each_item, list):
+#             print_lol(each_item)
+#         else:
+#             print(each_item)
+
+
+print_lol(movies)
